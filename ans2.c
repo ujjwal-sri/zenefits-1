5e8aeb6d54a0f60ee16e219b3fbd54f35c51b625
VIVEKDH <38081924+VIVEKDH@users.noreply.github.com>
2018-04-05 19:32:31 +0530

    Add files via upload

---
+++ ans2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	int size;
+	int c;
+	char a[];
+	string name;
+	string result[];
+	
+	for (i=0,i<size,i++){
+	    if int(a[i]) > int(a[i+1])
+	       { c=a[i];
+	         a[i]=a[i+1];
+	         a[i+1]=c;
+	           
+	           
+	           
+	    }
+	    char b[];
+	    
+	    
+	    for(j=0,j<(size)!,j++)
+	    { for(k=0,k<(size-j)!,k++)
+	        {for(s=j,s<(size-j),s++){
+	            if(a[s]>a[s+1]){
+	                c=a[s]
+	            }
+	        }
+	        
+	        
+	        
+	        }
+	        
+	        b[j]=a[j];
+	        
+	        
+	    }
+	    
+	    
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
