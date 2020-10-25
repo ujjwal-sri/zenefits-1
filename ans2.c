21daade209a9a234e83511f8afe57050e907b548
Satnam412 <37145383+Satnam412@users.noreply.github.com>
2020-10-25 18:25:22 +0530

    New Solution added

--- ans2.c
+++ ans2.c
@@ -1,49 +1,41 @@
 #include <stdio.h>
 #include <math.h>
-
-int main(void) {
-	int size;
-	int c;
-	char a[];
-	string name;
-	string result[];
-	
-	for (i=0,i<size,i++){
-	    if int(a[i]) > int(a[i+1])
-	       { c=a[i];
-	         a[i]=a[i+1];
-	         a[i+1]=c;
-	           
-	           
-	           
-	    }
-	    char b[];
-	    
-	    
-	    for(j=0,j<(size)!,j++)
-	    { for(k=0,k<(size-j)!,k++)
-	        {for(s=j,s<(size-j),s++){
-	            if(a[s]>a[s+1]){
-	                c=a[s]
-	            }
-	        }
-	        
-	        
-	        
-	        }
-	        
-	        b[j]=a[j];
-	        
-	        
-	    }
-	    
-	    
-	
-	
-	
-	
-	
-	
-	return 0;
+#include<string.h>
+int main(){
+        int i,n,j,count=0,k=0,l=0;
+        scanf("%d",&n);
+        char letrss[10000];
+        for(i=0;i<n;i++)
+        {
+            scanf("%c",letrss[i]);
+        }
+        char reqrd[10000];
+        scanf("%s",&reqrd);
+        int facti = factorial(n);
+        char name[facti][n];
+        for(i=0;i<facti;i++){
+            for(j=i+1;j<n;j++){
+                if(letrss[i]<=letrss[j]){
+                name[k][l]=letrss[i];
+                }
+                else{
+                    name[k][l]=letrss[j];
+                }
+                l++;
+            }
+            k++;l=0;
+            if(k==facti) break;
+        }
+        for(i=0;i<facti;i++){
+            if(strcmp(name[i],reqrd)==0){
+                printf("%d",i+1);
+                break;
+            }
+        }
+    return 0;
 }
+int factorial(int a){
+    if(a==0) return 1;
+    else return (a*factorial(a-1));
 
+}
